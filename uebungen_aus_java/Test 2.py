c7c8ca9f29add088afc887fc218ef8f8e1454ccd
Flor3nc3P <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2023-12-16 19:57:33 +0100

    Testing

---
+++ uebungen_aus_java/Test 2.py
@@ -0,0 +1,13 @@
+# Java Übung 1 in Python
+# Die while Bedingung wird mit : beendet
+# es kann gleich ein Wert zugewiesen werden
+# print dient zur Ausgabe und meine Varibale ist nach dem , zu schreiben
+# Der Inhalt der Schleife muss eine Tab beinhalten
+
+nummer = 9
+
+while nummer >= 0:
+    print("Zahl: ", nummer)
+    nummer = nummer - 1
+    
+
