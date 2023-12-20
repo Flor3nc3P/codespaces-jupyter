1a2702ad419d899aa009face05185c34cc999f90
Flor3nc3P <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2023-12-20 12:59:32 +0100

    school stuff

---
+++ projects/for_loop_18_12.py
@@ -0,0 +1,72 @@
+#P
+#P Y
+#P Y T
+#P Y T H
+#P Y T H O
+#P Y T H O N
+
+#wort = "PYTHON"
+
+#for i in range(1, len(wort) + 1):
+#    print(wort[:i])
+
+
+#wort = "Ausgabe"
+
+#for i in range(1, len(wort)+1):
+#    print(wort[:i])
+
+
+
+#for i in range(6):
+#    print(" " * (6 - i) + str(i) * i)
+
+
+
+
+#wort = "xxxxxx"
+
+#for i in range(1, len(wort) + 1):
+ #   print (wort[:i])
+
+#for j in range(6, len(wort) - 1):
+ #   print (wort[:j])
+
+
+
+import turtle
+#Ein Kreis mit einer for-Schleife
+for _ in range(36):  # 40 Schritte für einen "Kreis"
+    turtle.forward(20)  # Vorwärtsbewegung um 20 Einheiten
+    turtle.left(10)  # Drehung um 5 Grad
+
+
+turtle.done()
+
+#import turtle
+#turtle = turtle.Turtle()
+
+
+
+#Übung Chat
+
+#import turtle
+#wn = turtle.Screen()
+#wn.bgcolor("yellow")
+#skk=turtle.Turtle()
+#skk.color("black")
+
+#def sqrfunc(size):
+#    for i in range(4):
+#        skk.fd(size)
+#        skk.left(90)
+#        size = size +5
+
+#sqrfunc(60)        
+
+
+
+
+
+
+
