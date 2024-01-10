8f2bd6eeb22d54f83ed356c9687dc894d139b2a4
Flor3nc3P <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2024-01-10 22:16:07 +0100

    Sternchenmuster

--- uebungen_aus_java/sternchenmuster.py
+++ uebungen_aus_java/sternchenmuster.py
@@ -1,30 +1,87 @@
-#Ausgabe soll eine Seite eines auf und absteigenden * Musters sein.
-#In der FOR-Schleife (Initialisierung; Bedingung; Modifikation)
-#Die FOR-Schleife ist eine Kopfgesteuerte Schleife oder auch eine abweisende Schleife.
-#Die Schleife kann durch die Bedingung gleich am Anfang abgebrochen werden, wenn die Bedingung erfüllt ist.
-#Bedeutet Sie durchläuft nicht zwingend den Rumpf bzw. die Befehle die im Schleifenkörper stehen. Hier die Print Befehle.
-#Ich kann mit "" und '' printen. Vorteil von '' ist, das ich in meinem String "" verweden kann ohne das Python verwirrt ist.
-#Mit ''' kann ich Mehrzeilige Strings in der print Funktion schreiben.
-#Die input() Funktion in verbindung mit der print(f'') Funktion erlaubt es mir, meinen Überschrift variabel zu gestalten.
-#Mit der Type Casting Funktion kann ich meinen Datentyp in der Input Funktion festlegen.
+# Ausgabe soll eine Seite eines auf und absteigenden * Musters sein.
+# In der FOR-Schleife (Initialisierung; Bedingung; Modifikation)
+# Die FOR-Schleife ist eine Kopfgesteuerte Schleife oder auch eine abweisende Schleife.
+# Die Schleife kann durch die Bedingung gleich am Anfang abgebrochen werden, wenn die Bedingung erfüllt ist.
+# Bedeutet Sie durchläuft nicht zwingend den Rumpf bzw. die Befehle die im Schleifenkörper stehen. Hier die Print Befehle.
+# Ich kann mit "" und '' printen. Vorteil von '' ist, das ich in meinem String "" verweden kann ohne das Python verwirrt ist.
+# Mit ''' kann ich Mehrzeilige Strings in der print Funktion schreiben.
+# Die input() Funktion in verbindung mit der print(f'') Funktion erlaubt es mir, meinen Überschrift variabel zu gestalten.
+# Mit der Type Casting Funktion kann ich meinen Datentyp in der Input Funktion festlegen.
 
+
+# Titel für Muster eintippen
 titel_für_muster = (input("Gib einen passenden Titel ein: \n"))
 print(f'''Mein 
       "{titel_für_muster}" :) ''')
 
 
-zeile=0
-spalte=0
+# Figur 1 Orininalmuster
+# Sternchen hoch
+for zeile in range(10):
+    for spalte in range(zeile + 1):
+        print("x", end="")
+    print()
 
-for  zeile in zeile<10, zeile+1:
-    for spalte in spalte<zeile+1, spalte+1:
-        print("*\n")
-
-zeile=9
-spalte=9
-
-for zeile in zeile>0, zeile-1:
-     for spalte in spalte>zeile, spalte-1:
-        print("*\n")
+# Sternchen runter
+for zeile in range(9, 0, -1):
+    for spalte in range(zeile):
+        print("x", end="")
+    print()
 
 
+
+# Originalmuster links vertikal gespiegelt
+anzahlZeilen = 21
+anzahlX = 0
+
+for zeile in range(anzahlZeilen):
+    if zeile <= anzahlZeilen // 2:
+        anzahlX += 2
+    else:
+        anzahlX -= 2
+    
+    anzahlLeerzeichen = ((anzahlZeilen + 1) - anzahlX) // 2
+    
+    for n in range(anzahlLeerzeichen):
+        print(" ", end="")
+    for n in range(anzahlX):
+        print("x", end="")
+    
+    print()
+
+
+
+# Originalmuster rechts vertikal gespiegelt 
+# Originalmuster
+for zeile in range(10):
+    for spalte in range(zeile + 1):
+        print("x", end="")
+        
+        # Spiegelachse
+    for spalte in range(9 - zeile):
+        print(" ", end="")
+    print(end=" | ")
+    for spalte in range(9 - zeile):
+        print(" ", end="")
+        # Spiegelachse ende
+
+    for spalte in range(zeile + 1):
+        print("x", end="")
+    print()
+
+# Gespiegeltes Originalmuster
+for zeile in range(9, 0, -1):
+    for spalte in range(zeile):
+        print("x", end="")
+
+        # Spiegelachse
+    for spalte in range(10 - zeile):
+        print(" ", end="")
+    print(end=" | ")
+    for spalte in range(10 - zeile):
+        print(" ", end="")
+        #Spiegelachse ende
+
+    for spalte in range(zeile):
+        print("x", end="")
+    print()
