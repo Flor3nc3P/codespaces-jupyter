311a1d9060fe833e35f96e5ac3c63f95bbc44063
Flor3nc3P <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2024-01-20 17:31:34 +0100

    testing_and_learning_classes

--- tests/testing_and_learning2.py
+++ tests/testing_and_learning2.py
@@ -1,6 +1,7 @@
 # programmieren starten Video 22/24 Klasse  und Objekte
 # __init__ ist eine Funktion (bzw. Methode, da innerhalb einer Klasse definiert)
 # Methoden haben einen (self) Parameter, Funktionen nicht
+# self beinhaltet eine Referenz auf das aufrufende Objekt
 # def definiert eine Funktion/ wird vor die Funktion geschrieben
 
 
@@ -12,6 +13,8 @@ class Car:
         self.horse_power=None
         self.color=None
         self.car_model=None	
+        self.x_position = 5
+        self.y_position = 5
 
 # Referenz auf die Klasse Auto in Variable car1 gespeichert
         
@@ -33,5 +36,21 @@ car2.horse_power = 460
 car2.color = "Metallic ROT"
 
 print(car2.car_brand,car2.car_model,car2.horse_power,car2.horse_power)
+
+# Gibt den Speicherbereich/Referenz/Adresse in dem alle Daten des erzeugten Objekts gespeichert sind
+print(car1)
+print(car2)
+
+# car3 speichert die Referenz aus car1 und hat dementsprechend genau die selbe Referenz wie car1
+car3 = car1
+print(car3)
+
+
+def drive(self,x,y):
+    self.x_position += x
+    self.y_position += y
+
+
+
 
 
