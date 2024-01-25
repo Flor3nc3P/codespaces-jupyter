fb09018996f1a9a39c5d7e6c909c02aa792c945f
Flor3nc3P <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2024-01-25 12:59:08 +0100

    Fail aus Java

---
+++ uebungen_aus_java/zahlen_sortieren.py
@@ -0,0 +1,40 @@
+import math
+import time
+import numpy
+# import counter
+import random
+
+
+f=1000
+i=0
+
+if i<f.length:
+    i+1
+    f[i]=math.random *9999 +1
+
+print(f)
+print("\n Sortieren: \n")
+long = before = time()
+numpy.sort(f)
+long = after = time()
+
+print(f)
+print(f" {after-long} Zeit")
+# print(f" Der Vergleichszähler steht auf: {counter}")
+
+# j = int
+# m = int
+# i = int
+# x=[]
+
+# if i==1 and i<x:
+#     i=+1
+#     j=i
+#     m=[i]
+#     while (j>0 and [j-1] >m):
+#         [j]=[j-1]
+#         j-1
+#     [j]=m
+        
+
+
