4ed3fde3bc3f5d89c39c7d043f4152f9320206ba
Flor3nc3P <flo.pfitzinger.23@lehre.mosbach.dhbw.de>
2024-02-07 09:30:13 +0100

    Tutorial None

---
+++ tutorials/None.py
@@ -0,0 +1,29 @@
+# is vergleicht ob es sich um das gleiche Objekt handelt
+# == vergleicht ob es sich um den gleichen Inhalt handelt
+
+variable=None
+print(variable)
+print(type(variable))
+
+## Testabfragen:
+
+if variable == True:
+    print("None is True")
+elif variable == False:
+    print("None is False")
+elif variable == 0:
+    print("")
+elif variable == "":
+    print ("")
+elif variable in None:
+    print("None ist nur None")
+
+# Funktionen die nichts zurück geben, geben tatsächlich None zurück
+def GibNoneZurueck():
+    print("Funktion wird ausgeführt")
+
+test = GibNoneZurueck()
+print(test)
+
+# Mit None kann nicht gerechnet werden!
+print(1 + None)
